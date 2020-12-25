ac605efd83e285734793f4465b6b61e4baa910c6
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-12-25 09:32:13 +0700

    add qr dependencies

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default class BasicExample extends React.Component {
   render() {
-    return <LottieView source={require('./animation.json')} autoPlay loop />;
+    return (
+      <>
+        <QRCode
+          size={200}
+          value='
+          {
+    "id": "number",
+    "nama_lengkap": "string",
+    "email": "string <email>",
+    "telepon": "string",
+    "lokasi": "string",
+    "avatar": "string <uri>",
+}
+          '
+        />
+        <LottieView source={require('./animation.json')} autoPlay loop />
+      </>
+    );
   }
 }
