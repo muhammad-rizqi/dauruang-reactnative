2d17d5f72a4aa177e5bb2ef7e2aa70b9ce1ba85b
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-12-25 09:23:59 +0700

    add lottie

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import ScanScreen from './src/QRscan';
-import Maps from './src/Maps';
-const App = () => {
-  return (
-    <View>
-      <ScanScreen />
-    </View>
-  );
-};
+import LottieView from 'lottie-react-native';
 
-export default App;
+export default class BasicExample extends React.Component {
+  render() {
+    return <LottieView source={require('./animation.json')} autoPlay loop />;
+  }
+}
