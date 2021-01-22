adc1ee274fbd3d4005c6fee7cbf5b412e9e807c8
Muhammad Rizqi <muhammad-rizqi@live.com>
2021-01-22 10:20:32 +0700

    change splash

--- src/screen/Auth/Splash.js
+++ src/screen/Auth/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View, Text, StatusBar, ActivityIndicator} from 'react-native';
 import Logo from '../../assets/img/logo.svg';
 import {colors, styles} from '../../style/styles';
 
@@ -9,6 +9,16 @@ const Splash = () => {
       <StatusBar backgroundColor={colors.lightBg} barStyle="dark-content" />
       <Logo width={120} height={120} />
       <Text style={[styles.textH2, styles.textPrimary]}>Daur Uang</Text>
+      <Text
+        style={[
+          styles.textH3,
+          styles.textPrimary,
+          styles.textCenter,
+          styles.marginVXL,
+        ]}>
+        Tukar sampah jadi uang, {'\n'} selamatkan bumi sambil investasi
+      </Text>
+      <ActivityIndicator color={colors.primary} size="large" />
     </View>
   );
 };
